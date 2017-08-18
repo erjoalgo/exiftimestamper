7ca4a5aaa01e39a8cc263e5e714c8b222f1ddb53
Ernesto Alfonso <erjoalgo@gmail.com>
2017-08-18 07:05:23 -0700

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+sudo rm -rf build  dist *.egg-info
+python setup.py sdist
+VERSION=$(cat VERSION | head -1 | tr -d '\n')
+PROJECT_NAME=$(basename $(pwd))
+TARGZ=dist/${PROJECT_NAME}-${VERSION}.tar.gz
+test -e ${TARGZ} || exit ${LINENO}
+gpg --detach-sign -a ${TARGZ} || exit ${LINENO}
+gpg --verify -a ${TARGZ}{.asc,} || exit ${LINENO}
+
+if ! command -v twine; then
+    pip install --user twine
+fi
+
+CMD=$(echo twine upload ${TARGZ}{,.asc})
+read -p "${CMD} :"
+${CMD}
